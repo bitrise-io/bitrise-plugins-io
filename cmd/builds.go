8a07ceba0699c513c5d3af89a201f5b5b8a71817
Viktor Benei <viktor.benei@gmail.com>
2018-05-20 23:32:57 +0200

    Build Command output revision
    - fixed next, limit, sort flag reuse issue -> DO NOT REUSE FLAG VARIABLES, declare new ones, otherwise default values might not work!
    - formatted Build list output, using GO TabWriter

--- cmd/builds.go
+++ cmd/builds.go
@@ -1,15 +1,24 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
 	"os"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/bitrise-core/bitrise-plugins-io/services"
+	"github.com/bitrise-io/go-utils/colorstring"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 var (
-	appSlugFlag string
+	appSlugFlag     string
+	buildsSortFlag  string
+	buildsNextFlag  string
+	buildsLimitFlag string
 )
 
 var buildsCmd = &cobra.Command{
@@ -25,18 +34,78 @@ var buildsCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(buildsCmd)
-	buildsCmd.Flags().StringVar(&nextFlag, "next", "", "Next parameter for paging")
-	buildsCmd.Flags().StringVarP(&limitFlag, "limit", "l", "", "Limit parameter for paging")
-	buildsCmd.Flags().StringVar(&sortFlag, "sort", "", "Sort by parameter for listing")
+	buildsCmd.Flags().StringVar(&buildsNextFlag, "next", "", "Next parameter for paging")
+	buildsCmd.Flags().StringVarP(&buildsLimitFlag, "limit", "l", "", "Limit parameter for paging")
+	buildsCmd.Flags().StringVar(&buildsSortFlag, "sort", "created_at", "Sort by parameter for listing. Options: [created_at, running_first]")
 	buildsCmd.Flags().StringVarP(&appSlugFlag, "app", "a", "", "Slug of the app where the builds belong to")
-	buildsCmd.Flags().StringVar(&formatFlag, "format", "pretty", "Output format, one of: [pretty, json]")
+}
+
+// BuildsListReponseModel ...
+type BuildsListReponseModel struct {
+	Data []struct {
+		Slug          string `json:"slug"`
+		Status        int    `json:"status"`
+		StatusText    string `json:"status_text"`
+		IsOnHold      bool   `json:"is_on_hold"`
+		BuildNumber   int    `json:"build_number"`
+		CommitHash    string `json:"commit_hash"`
+		CommitMessage string `json:"commit_message"`
+		PullRequestID int    `json:"pull_request_id"`
+	} `json:"data"`
+}
+
+func coloredStatusText(statusText string) string {
+	switch statusText {
+	case "success":
+		return colorstring.Green(statusText)
+	case "error":
+		return colorstring.Red(statusText)
+	case "aborted":
+		return colorstring.Yellow(statusText)
+	}
+	return statusText
+}
+
+func prettyBuildMessageText(msg string) string {
+	s := fmt.Sprintf("%.50s", msg)         // print the first X chars
+	s = strings.Replace(s, "\n", "↲", -1)  // replace newlines
+	s = strings.Replace(s, "\r", "↲", -1)  // replace newlines
+	s = strings.Replace(s, "\t", "  ", -1) // replace tabs
+	return s
+}
+
+func prettyPR(prID int) string {
+	if prID == 0 {
+		// empty string if zero
+		return ""
+	}
+	return fmt.Sprintf("%d", prID)
+}
+
+// Pretty ...
+func (respModel *BuildsListReponseModel) Pretty() string {
+	buf := bytes.NewBuffer([]byte{})
+	prettyTabWriter := tabwriter.NewWriter(buf, 0, 0, 1, ' ', 0)
+	fmt.Fprintln(prettyTabWriter, "BuildNum\t"+colorstring.Blue("Status")+"\tSlug\tPullRequestID\tMessage")
+	for _, aItem := range respModel.Data {
+		fields := []string{
+			fmt.Sprintf("%d", aItem.BuildNumber),
+			coloredStatusText(aItem.StatusText),
+			aItem.Slug,
+			prettyPR(aItem.PullRequestID),
+			prettyBuildMessageText(aItem.CommitMessage),
+		}
+		fmt.Fprintln(prettyTabWriter, strings.Join(fields, "\t"))
+	}
+	prettyTabWriter.Flush()
+	return buf.String()
 }
 
 func builds() error {
 	params := map[string]string{
-		"next":    nextFlag,
-		"limit":   limitFlag,
-		"sort_by": sortFlag,
+		"next":    buildsNextFlag,
+		"limit":   buildsLimitFlag,
+		"sort_by": buildsSortFlag,
 	}
 
 	response, err := services.GetBitriseBuildsForApp(appSlugFlag, params)
@@ -49,6 +118,6 @@ func builds() error {
 		os.Exit(1)
 		return nil
 	}
-	printOutput(response.Data, formatFlag != "json")
-	return nil
+
+	return errors.WithStack(printOutputWithPrettyFormatter(response.Data, formatFlag != "json", &BuildsListReponseModel{}))
 }
